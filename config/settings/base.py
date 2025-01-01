19c2d65551d33a5fc67e0b05f665c0948e01966f
clab-to <73862233+clab-to@users.noreply.github.com>
2025-01-01 05:46:50 +0000

    p371 モデルをルーティングできるようにする

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from api.inventory.apps import NAME as INVENTORY_NAME
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "api.hello_db",
+    INVENTORY_NAME
 ]
 
 MIDDLEWARE = [
