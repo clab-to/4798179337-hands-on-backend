a007d40cdb8ebc31fe8a8f4079e85c445b71d010
clab-to <73862233+clab-to@users.noreply.github.com>
2025-01-02 03:45:52 +0000

    p.416 CRUD実装

--- api/inventory/urls.py
+++ api/inventory/urls.py
@@ -2,8 +2,15 @@ from django.urls import path
 
 from api.inventory.views import ProductModelViewSet
 from api.inventory.views import ProductView
+from api.inventory.views import PurchaseView
+from api.inventory.views import SalesView
 
 urlpatterns = [
     path("products/", ProductView.as_view()),
-    path("products/model", ProductModelViewSet.as_view({"get": "list"})),
+    path("products/<int:_id>", ProductView.as_view()),
+    path(
+        "products/model", ProductModelViewSet.as_view({"get": "list", "post": "create"})
+    ),
+    path("purchases/", PurchaseView.as_view()),
+    path("sales/", SalesView.as_view()),
 ]
--- api/inventory/views.py
+++ api/inventory/views.py
@@ -1,30 +1,87 @@
 from rest_framework import status
 from rest_framework import views
 from rest_framework import viewsets
+from rest_framework.exceptions import NotFound
 from rest_framework.request import Request
 from rest_framework.response import Response
 
 from api.inventory.models import Product
 from api.inventory.serializers import ProductSerializer
+from api.inventory.serializers import PurchaseSerializer
+from api.inventory.serializers import SalesSerializer
 
 
 class ProductView(views.APIView):
     """商品操作に関する関数"""
 
-    def get(self, request: Request, format=None) -> Response:
-        """商品一覧を取得する"""
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
+    def __init__(self) -> None:
+        super().__init__()
+        self._serializer = ProductSerializer
+
+    def get_object(self, primary_key: int) -> Product:
+        """idとprimary_keyが一致する1つの商品を取得する"""
+        try:
+            return Product.objects.get(pk=primary_key)
+        except Product.DoesNotExist as e:
+            raise NotFound from e
+
+    def get(self, request: Request, _id: int | None = None, format=None) -> Response:
+        """商品一覧、もしくは一意の商品を取得する"""
+        if _id is None:
+            queryset = Product.objects.all()
+            serializer = self._serializer(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        product = self.get_object(_id)
+        serializer = self._serializer(product)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request: Request) -> Response:
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request: Request, format=None) -> Response:
+        """商品を新規登録する"""
+        serializer = self._serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def put(self, request: Request, _id: int, format=None) -> Response:
+        """登録済みの商品を更新する"""
+        product = self.get_object(_id)
+        serializer = self._serializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request: Request, _id: int, format=None) -> Response:
+        """登録済みの商品を削除する"""
+        product = self.get_object(_id)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class ProductModelViewSet(viewsets.ModelViewSet):
+    """商品操作に関する関数（ModelViewSet）"""
+
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class PurchaseView(views.APIView):
+    """仕入操作に関する関数"""
+
+    def post(self, request: Request, format=None) -> Response:
+        """仕入を登録する"""
+        serializer = PurchaseSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class SalesView(views.APIView):
+    """売上操作に関する関数"""
+
+    def post(self, request: Request, format=None) -> Response:
+        """売上を登録する"""
+        serializer = SalesSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
